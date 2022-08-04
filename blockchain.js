81ef8e6e342dac85b8fe8915bca1d717c81a7476
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-04 15:57:35 -0400

    Implement static `isValidChain()` method

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,27 @@
 const Block = require('./block');
+const cryptoHash = require('./crypto-hash');
+
+/**
+ * The goal of the blockchain network is that they all come to a 
+ * unanimous agreement on the true longest and valid blockchain.
+ * 
+ * And this is the power of the blockchain. multiple nodes have now
+ * come to an agreement on an official set of blocks. That blockchain
+ * is now distributed and public across all the nodes. And everyone
+ * has an equal stake in maintaining that valid blockchain. Plus, its
+ * decentralized. There's not one entity with the responsibility of 
+ * managing all of the data and therefore having all the power of the
+ * entire system.
+ * 
+ * All of this wouldn't be possible without chain validation and 
+ * replacement
+ * 
+ * The power of the blockchain shines when multiple instances of a 
+ * blockchain application works together like a wolfpack.
+ * 
+ * When multiple blockchains are interacting like this, we're calling
+ * this the blockchain network.
+ */
 
 class Blockchain {
   constructor() {
@@ -12,6 +35,26 @@ class Blockchain {
     });
 
     this.chain.push(newBlock);
+  }
+
+  static isValidChain(chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false;
+    }
+
+    for (let i = 1; i < chain.length; i++) {
+      const { timestamp, lastHash, hash, data } = chain[i];
+
+      const actualLastHash = chain[i-1].hash;
+
+      if (lastHash !== actualLastHash) return false;
+
+      const validatedHash = cryptoHash(timestamp, lastHash, data);
+
+      if (hash !== validatedHash) return false;
+    }
+
+    return true;
   }
 }
 
