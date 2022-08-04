bd2acbd1f0b6592800fbb3e2cd899895168512d4
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-04 00:16:21 -0400

    Create config file for storing hard-coded and global values

---
+++ config.js
@@ -0,0 +1,10 @@
+// Store hard-coded and global values here
+
+const GENESIS_DATA = {
+  timestamp: 1, 
+  lastHash: '-----',
+  hash: 'hash-one',
+  data: []
+};
+
+module.exports = { GENESIS_DATA };
