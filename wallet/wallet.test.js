7664ff9d413138d044e00cb83f74380424f74675
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-05 14:02:16 -0400

    Define initial test suite for signing data

--- wallet/wallet.test.js
+++ wallet/wallet.test.js
@@ -1,4 +1,5 @@
 const Wallet = require('./wallet');
+const verifySignature = require('../util/verify-signature');
 
 describe('Wallet', () => {
   let wallet;
@@ -14,5 +15,29 @@ describe('Wallet', () => {
   it('has a `publicKey`', () => {
     console.log(wallet.publicKey);
     expect(wallet).toHaveProperty('publicKey');
+  });
+
+  describe('Signing data', () => {
+    const data = 'foobar';
+
+    it('verifies a signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data, 
+          signature: wallet.sign(data)
+        })
+      ).toBe(true);
+    });
+
+    it('does not verify an invalid signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: new Wallet().sign(data)
+        })
+      ).toBe(false);
+    });
   });
 });
