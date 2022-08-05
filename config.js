92d3ff7f95eaacae05f445cb998f92df142f8cfc
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-04 20:52:25 -0400

    Add `MINE_RATE` and `INITIAL_DIFFICULTY` global values to config file

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 // Store hard-coded and global values here
 
+// represents the rate at which we want new blocks to be added to the chain
+const MINE_RATE = 1000; // 1 sec. mine rate is set in millis.
+
+const INITIAL_DIFFICULTY = 3;
+
 const GENESIS_DATA = {
   timestamp: 1, 
   lastHash: '-----',
   hash: 'hash-one',
+  difficulty: INITIAL_DIFFICULTY,
+  nonce: 0,
   data: []
 };
 
