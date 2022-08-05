2dd85a7acd8140af386a776bcadf8571a1970515
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-05 00:59:53 -0400

    Add ability to sync chains and start peers via alt ports

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const bodyParser = require('body-parser');
 const Block = require('./block');
 const Blockchain = require('./blockchain');
@@ -8,7 +9,10 @@ const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
 
-setTimeout(() => pubsub.broadcastChain(), 1000);
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+
+//setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(bodyParser.json());
 
@@ -21,8 +25,33 @@ app.post('/api/mine', (req, res) => {
 
   blockchain.addBlock({data});
 
+  pubsub.broadcastChain();
+
   res.redirect('/api/blocks');
 });
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`listening at localhost:${PORT}`));
+const syncChains = () => {
+  request({ url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const rootChain = JSON.parse(body);
+
+      console.log('replace chain on a sync with', rootChain);
+      blockchain.replaceChain(rootChain);
+    }
+  });
+};
+
+let PEER_PORT;
+
+if (process.env.GENERATE_PORT === 'true') {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
+app.listen(PORT, () => {
+  console.log(`listening at localhost:${PORT}`);
+  
+  if (PORT !== DEFAULT_PORT) {
+    syncChains();
+  }
+});
