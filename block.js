2884df9e58709664466bd05c95a3e85683ef00a4
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-04 22:07:46 -0400

    Adjust the difficulty based on the binary version of hash

--- block.js
+++ block.js
@@ -1,3 +1,4 @@
+const hexToBinary = require('hex-to-binary');
 const { GENESIS_DATA, MINE_RATE } = require('./config');
 const cryptoHash = require('./crypto-hash');
 
@@ -40,20 +41,28 @@ class Block {
   static mineBlock({ lastBlock, data }) {
     let hash, timestamp;
     const lastHash = lastBlock.hash;
-    const { difficulty } = lastBlock;
+    let { difficulty } = lastBlock;
     let nonce = 0;
+
+    // The difficulty for each newly mined block will adjust based on how long it took to
+    // find the valid hash compared to the MINE_RATE.
 
     do {
       nonce++;
       timestamp = Date.now();
+      difficulty = Block.adjustDifficulty({originalBlock: lastBlock, timestamp })
       hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty)
-    } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
+    } while (hexToBinary(hash).substring(0, difficulty) !== '0'.repeat(difficulty));
 
-    return new this({timestamp,lastHash,data,difficulty,nonce,hash});
+    return new this({timestamp, lastHash, data, difficulty, nonce, hash});
   }
 
   static adjustDifficulty({ originalBlock, timestamp }) {
     const { difficulty } = originalBlock;
+
+    if (difficulty < 1) {
+      return 1;
+    }
 
     if ((timestamp - originalBlock.timestamp) > MINE_RATE) {
        return difficulty - 1;
