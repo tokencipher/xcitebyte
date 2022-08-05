2e1e150961847b020ae9faa8d59f693215ce52ad
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-04 22:09:01 -0400

    Set hash that matches the difficulty criteria based on binary version

--- block.test.js
+++ block.test.js
@@ -1,3 +1,4 @@
+const hexToBinary = require('hex-to-binary');
 const Block = require('./block');
 const { GENESIS_DATA, MINE_RATE } = require('./config');
 const cryptoHash = require('./crypto-hash');
@@ -87,8 +88,14 @@ describe('Block', () => {
     });
 
     it('sets a `hash` that matches the difficulty criteria', () => {
-      expect(minedBlock.hash.substring(0, minedBlock.difficulty))
+      expect(hexToBinary(minedBlock.hash).substring(0, minedBlock.difficulty))
         .toEqual('0'.repeat(minedBlock.difficulty));
+    });
+
+    it('adjusts the difficulty', () => {
+      const possibleResults = [lastBlock.difficulty+1, lastBlock.difficulty-1];
+
+      expect(possibleResults.includes(minedBlock.difficulty)).toBe(true);
     });
   });
 
@@ -111,5 +118,11 @@ describe('Block', () => {
       })).toEqual(block.difficulty - 1);
     });
     
+
+    it('has a lower limit of 1', () => {
+      block.difficulty = -1;
+
+      expect(Block.adjustDifficulty({ originalBlock: block })).toEqual(1);
+    });
   });
 });
