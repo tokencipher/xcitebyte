6fd7e95b9f90d3715fe63714080b34c884a3ce8d
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-04 23:55:18 -0400

    Define /api/mine route handler for POST requests

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Block = require('./block');
 const Blockchain = require('./blockchain');
 
 const app = express();
 const blockchain = new Blockchain();
 
+app.use(bodyParser.json());
+
 app.get('/api/blocks', (req, res) => {
   res.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req, res) => {
+  const { data } = req.body;
+
+  blockchain.addBlock({data});
+
+  res.redirect('/api/blocks');
 });
 
 const PORT = 3000;
