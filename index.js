4b17b337e44f725773df45b1c43d4108eed0e82f
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-06 17:29:05 -0400

    Define `/api/transaction-pool-map` GET route

--- index.js
+++ index.js
@@ -51,9 +51,14 @@ app.post('/api/transact', (req, res) => {
   }
 
   transactionPool.setTransaction(transaction);
+
+  //console.log('transactionPool', transactionPool);
   
-  console.log('transactionPool', transactionPool);
   res.json({type: 'success', transaction});
+});
+
+app.get('/api/transaction-pool-map', (req, res) => {
+  res.json(transactionPool.transactionMap);
 });
 
 const syncChains = () => {
