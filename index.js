11917267210bdd3bbea3c814771f4b390fc76b26
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-06 13:24:55 -0400

    Define `/api/transact` endpoint

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const bodyParser = require('body-parser');
 const Block = require('./blockchain/block');
 const Blockchain = require('./blockchain/blockchain');
 const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet/wallet');
 
 const app = express();
 const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain });
 
 const DEFAULT_PORT = 3000;
@@ -28,6 +32,18 @@ app.post('/api/mine', (req, res) => {
   pubsub.broadcastChain();
 
   res.redirect('/api/blocks');
+});
+
+app.post('/api/transact', (req, res) => {
+  const {amount, recipient} = req.body;
+
+  const transaction = wallet.createTransaction({recipient, amount});
+
+  transactionPool.setTransaction(transaction);
+
+  console.log('transactionPool', transactionPool);
+
+  res.json({transaction});
 });
 
 const syncChains = () => {
