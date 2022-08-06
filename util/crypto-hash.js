fb95e254392d34e45ef2a2fb17b9f4932f88e860
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-05 21:23:01 -0400

    Stringify input to hash to reflect modified object

--- util/crypto-hash.js
+++ util/crypto-hash.js
@@ -3,7 +3,9 @@ const crypto = require('crypto');
 const cryptoHash = (...inputs) => {
   const hash = crypto.createHash('sha256');
 
-  hash.update(inputs.sort().join(' '));
+  // we're mapping over the inputs and turning the inner items to their 
+  // stringified form
+  hash.update(inputs.map(input => JSON.stringify(input)).sort().join(' '));
 
   // `Digest` is a term in cryptography that represents the result of a hash.
   return hash.digest('hex');
