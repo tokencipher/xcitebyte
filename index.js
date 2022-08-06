467796a998b61007169296ce2411a35430f3b90c
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-06 18:01:20 -0400

    Broadcast Transaction

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
-const pubsub = new PubSub({ blockchain });
+const pubsub = new PubSub({ blockchain, transactionPool });
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -53,7 +53,9 @@ app.post('/api/transact', (req, res) => {
   transactionPool.setTransaction(transaction);
 
   //console.log('transactionPool', transactionPool);
-  
+
+  pubsub.broadcastTransaction(transaction);
+
   res.json({type: 'success', transaction});
 });
 
