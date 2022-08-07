d9def3dd1e6dc63e10a41cc4aa0cb3b3aca93c90
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-07 00:11:45 -0400

    Refactor replaceChain() method to accept callback

--- blockchain/blockchain.js
+++ blockchain/blockchain.js
@@ -78,7 +78,7 @@ class Blockchain {
     this.chain.push(newBlock);
   }
 
-  replaceChain(chain) {
+  replaceChain(chain, onSuccess) {
     if (chain.length <= this.chain.length) {
       console.error('The incoming chain must be longer')
       return;
@@ -89,6 +89,7 @@ class Blockchain {
       return;
     }
 
+    if (onSuccess) onSuccess();
     console.log('replacing chain with', chain);
     this.chain = chain;
   }
