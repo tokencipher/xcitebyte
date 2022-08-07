45f28625cca4778f5eabffa59a57de5538dd094d
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-07 12:52:13 -0400

    Implement `validTransactionData()` method

--- blockchain/blockchain.js
+++ blockchain/blockchain.js
@@ -1,5 +1,7 @@
 const Block = require('./block');
+const Transaction = require('../wallet/transaction');
 const cryptoHash = require('../util/crypto-hash');
+const { REWARD_INPUT, MINING_REWARD } = require('../config');
 
 /**
  * The goal of the blockchain network is that they all come to a 
@@ -92,6 +94,38 @@ class Blockchain {
     if (onSuccess) onSuccess();
     console.log('replacing chain with', chain);
     this.chain = chain;
+  }
+
+  validTransactionData({chain}) {
+    for (let i = 1; i < chain.length; i++) {
+      const block = chain[i];
+      let rewardTransaction = 0;
+
+      for (let transaction of block.data) {
+        if (transaction.input.address = REWARD_INPUT.address) {
+          rewardTransaction += 1;
+
+          if (rewardTransaction > 1) {
+            console.error('Miner rewards exceed limit');
+            return false;
+          }
+
+          //console.log(Object.values(transaction.outputMap));
+
+          if (Object.values(transaction.outputMap)[0] !== MINING_REWARD) {
+            console.error('Miner reward amount is invalid');
+            return false;
+          }
+        } else {
+          if (!Transaction.validTransaction(transaction)) {
+            console.error('Invalid transaction');
+            return false;
+          }
+        }
+      }
+    }
+
+    return true;
   }
 
   static isValidChain(chain) {
