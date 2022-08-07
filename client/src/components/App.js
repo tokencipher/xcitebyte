205669b595527e4a251947b9b4e2f97d950d0b63
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-07 17:46:55 -0400

    Refactor client directory structure

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  // returns JSX
+  render() {
+    return (
+      <div>
+        Welcome to the blockchain...
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,22 @@
+// import the React module from its location at the react node module folder
+import React from 'react';
+
+// Render is not the default export of react-dom therefore we use curly braces.
+// This render function is the key to inserting react into the front-end, so 
+// it allows us to dynamically insert react components into the HTML document.
+import { render } from 'react-dom';
+
+// nodeJS, recall that we use the require keyword which is part of the system
+// called the commonJS project. However, the 2015 version of JavaScript, called
+// es6 added support for loading content through an import syntax
+
+import App from './components/App';
+
+//console.log('JavaScript hello!');
+
+// This mark-up is not actually HTML! 
+// The mark-up you are seeing here is JSX, short for JavaScript XML-like syntax
+render(
+  <App></App>,
+  document.getElementById('root')
+)
