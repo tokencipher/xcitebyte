29c364fb1e2056545b61b759c601d962dd5a4403
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-07 13:33:16 -0400

    Invalidate blocks with duplicate transactions

--- blockchain/blockchain.js
+++ blockchain/blockchain.js
@@ -100,6 +100,7 @@ class Blockchain {
   validTransactionData({chain}) {
     for (let i = 1; i < chain.length; i++) {
       const block = chain[i];
+      const transactionSet = new Set();
       let rewardTransaction = 0;
 
       for (let transaction of block.data) {
@@ -133,6 +134,13 @@ class Blockchain {
           if (transaction.input.amount !== trueBalance) {
             console.error('Invalid input amount');
             return false;
+          }
+
+          if (transactionSet.has(transaction)) {
+            console.error('An identical transaction appears more than once in the block')
+            return false;
+          } else {
+            transactionSet.add(transaction);
           }
         }
       }
