f0a0855bc5d2ed501bb1b88279a8fbad6ac63222
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-07 00:14:02 -0400

    Define test suite for calculateBalance() method

--- wallet/wallet.test.js
+++ wallet/wallet.test.js
@@ -1,6 +1,8 @@
 const Wallet = require('./wallet');
 const Transaction = require('./transaction');
 const verifySignature = require('../util/verify-signature');
+const Blockchain = require('../blockchain/blockchain');
+const { STARTING_BALANCE } = require('../config');
 
 describe('Wallet', () => {
   let wallet;
@@ -70,6 +72,57 @@ describe('Wallet', () => {
       it('outputs the amount to the recipient', () => {
         expect(transaction.outputMap[recipient]).toEqual(amount);
       });
+    });
+  });
+
+  describe('calculateBalance()', () => {
+    let blockchain;
+
+    beforeEach(() => {
+      blockchain = new Blockchain();
+    });
+
+    describe('and there are no outputs for the wallet', () => {
+      it('returns the `STARTING_BALANCE`', () => {
+        expect(
+          Wallet.calculateBalance({
+            chain: blockchain.chain,
+            address: wallet.publicKey
+          })
+        ).toEqual(STARTING_BALANCE);
+      });
+    });
+
+    describe('and there are outputs for the wallet', () => {
+      let transactionOne, transactionTwo;
+
+      beforeEach(() => {
+        transactionOne = new Wallet().createTransaction({
+          recipient: wallet.publicKey,
+          amount: 50
+        });
+
+        transactionTwo = new Wallet().createTransaction({
+          recipient: wallet.publicKey,
+          amount: 60
+        });
+
+        blockchain.addBlock({ data: [transactionOne, transactionTwo] });
+      });
+
+      it('adds the sum of all outputs to the wallet balance', () => {
+        expect(
+          Wallet.calculateBalance({
+            chain: blockchain.chain,
+            address: wallet.publicKey
+          })
+        ).toEqual(
+          STARTING_BALANCE +
+          transactionOne.outputMap[wallet.publicKey] +
+          transactionTwo.outputMap[wallet.publicKey]
+        )
+      });
+      
     });
   });
 });
