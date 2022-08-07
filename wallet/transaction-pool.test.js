a0fb2c68792368dbef87bc04997c34d0359d96db
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-06 20:09:09 -0400

    Define test suite for validTransactions() method

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -35,4 +35,44 @@ describe('TransactionPool', () => {
       ).toBe(transaction);
     });
   });
+
+  describe('validTransactions()', () => {
+    let validTransactions;
+
+    beforeEach(() => {
+      validTransactions = [];
+      errorMock = jest.fn();
+
+      // stub global.console.error to the errorMock
+      global.console.error = errorMock;
+
+      for (let i = 0; i < 10; i++) {
+        transaction = new Transaction({
+          senderWallet,
+          recipient: 'any-recipient',
+          amount: 30
+        });
+
+        if (i%3===0) {
+          transaction.input.amount = 999999;
+        } else if (i%3===1) {
+          transaction.input.signature = new Wallet().sign('foo');
+        } else {
+          validTransactions.push(transaction);
+        }
+
+        transactionPool.setTransaction(transaction);
+      }
+
+    });
+
+    it('returns valid transactions', () => {
+      expect(transactionPool.validTransactions()).toEqual(validTransactions);
+    });
+
+    it('logs errors for the invalid transactions', () => {
+      transactionPool.validTransactions();
+      expect(errorMock).toHaveBeenCalled();
+    });
+  });
 });
