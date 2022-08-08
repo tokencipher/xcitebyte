f12ce347bc2d5cd67b175cb400ca3c1b75886368
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-07 22:12:24 -0400

    Render Blocks component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Blocks from './Blocks';
 
 class App extends Component {
   state = { walletInfo: {}};
@@ -20,6 +21,8 @@ class App extends Component {
         <div>Welcome to the blockchain...</div>
         <div>Address: {address}</div>
         <div>Balance: {balance}</div>
+        <br />
+        <Blocks />
       </div>
     )
   }
