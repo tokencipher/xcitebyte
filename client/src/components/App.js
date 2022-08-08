e11b4f9f24ed782ec195c911808bde1a40de9e36
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-07 21:38:47 -0400

    Implement componentDidMount() lifecycle hook, fetching wallet info

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  state = { walletInfo: { address: 'fooxv6', balance: 9999 }};
+  state = { walletInfo: {}};
+
+  // fires as soon as the component has been inserted into the main document
+  // allows us to run the request without having to block the presentation appearing 
+  // to the user within the render method
+  componentDidMount() {
+    fetch('http://localhost:3000/api/wallet-info')
+      .then(response => response.json())
+      .then(json => this.setState({walletInfo: json}))
+  }
 
   // returns JSX
   render() {
