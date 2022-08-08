dbc8f8964646fe7bb526d090b011129f912994bd
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2022-08-07 21:12:38 -0400

    Add state to wallet, displaying mock wallet info

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = { walletInfo: { address: 'fooxv6', balance: 9999 }};
+
   // returns JSX
   render() {
+    const {address, balance} = this.state.walletInfo;
     return (
       <div>
-        Welcome to the blockchain...
+        <div>Welcome to the blockchain...</div>
+        <div>Address: {address}</div>
+        <div>Balance: {balance}</div>
       </div>
     )
   }
